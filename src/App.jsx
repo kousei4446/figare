86251920fb560589840e8289725a55ea4179c3a5
kousei <kousei20030623@icloud.com>
2024-06-28 17:09:05 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import Register from './components/newregester/Register';
+import Login from './components/Login/Login';
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Navigation() {
+  const location = useLocation();
+
+  // /aboutパス以降の場合はナビゲーションを表示しない
+  if (location.pathname.startsWith('/login')) {
+    return null;
+  }
+
+  return (
+    <nav>
+      <h1>Figare</h1>
+      <div>
+          <Link to="/login">ログイン</Link>
+          <br/>
+          <Link to="/login/register">新規登録</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
