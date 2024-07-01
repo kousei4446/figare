bfe575f748e933d2f8da4a93d4a13a44a62f2915
kondosoya <kondosoya0704@gmail.com>
2024-07-01 13:44:57 +0900

    detaillink

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation, useNavigate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation, useNavigate } from 'react-router-dom';
 import Register from './components/newregester/Register';
 import Login from './components/Login/Login';
 import "./App.css"
@@ -19,7 +19,7 @@ import Hito from './components/Home/AddPost/Hito';
 import SureHitoInfo from './components/Home/AddPost/SureHitoInfo';
 import Messeage from './components/Home/Message/Messeage';
 import Chat from './components/Home/Message/Chat';
-import FinderProfile from './components/Home/FinderProfile';
+import LostDetail from './components/LostDetail/LostDeatail';
 import Pet from './components/Home/AddPost/Pet';
 import SurePet from './components/Home/AddPost/SurePet';
 import Mono from './components/Home/AddPost/Mono';
@@ -54,8 +54,8 @@ function App() {
           <Route path="/login/home/addpost/mono/suremono" element={<SureMone />} />
 
           <Route path="/login/home/message" element={<Messeage />} />
-          <Route path="/login/home/chat" element={<Chat />} />          
-          <Route path="/login/home/finder" element={<FinderProfile />} />          
+          <Route path="/login/home/chat" element={<Chat />} />
+          <Route path="/login/home/finder" element={<LostDetail />} />
         </Routes>
       </div>
     </Router>
@@ -76,19 +76,12 @@ function Navigation() {
   }
 
   return (
-<<<<<<< HEAD
-    <nav>
+    <div>
       <h1>Figare</h1>
       <div>
         <Link to="/login">ログイン</Link>
         <br />
         <Link to="/login/register">新規登録</Link>
-=======
-    <div>
-      <div className='head'>
-        <h1 className="title">Figare</h1>
-        <h2 className='subtitle'>ログイン</h2>
->>>>>>> 9c13d1912513a6fcd1c1bb5d8e6317e03fe9995d
       </div>
       <nav className='mainlogin'>
         <div className='Input'>
@@ -96,17 +89,17 @@ function Navigation() {
             電話番号
             <input></input>
           </div>
-          <br/>
+          <br />
           <div className='input'>
             パスワード
             <input></input>
           </div>
         </div>
-        <br/>
+        <br />
         <div id="login">
           <button onClick={login} className='login'>ログイン</button>
         </div>
-          <br/>
+        <br />
         <div className='new'>
           <Link to="/login/register">新規登録の方はこちらへ</Link>
         </div>
