18366740521221342e1c2ab720035b2c7d1832ae
kousei <kousei20030623@icloud.com>
2024-07-01 14:42:08 +0900

    fdhuige

--- src/components/Home/AddPost/SureHitoInfo.jsx
+++ src/components/Home/AddPost/SureHitoInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import image from "../../img/image.png"
-
+import image1 from "../../img/img1.jpg"
 
 function SureHitoInfo() {
   const navigation = useNavigate()
@@ -16,8 +16,20 @@ function SureHitoInfo() {
       <div className='blue'></div>
       <img src={image} height="50px" className='back-btn' onClick={back} />
 
-      <h1>情報を確認してね​</h1>
-      <button onClick={ok}>確定する</button>
+      <p style={{ fontSize: "29px", marginTop: "0px",marginBottom:"0px", marginLeft: "5%" }}>情報を確認してね​</p>
+      <div className='con-text'>
+        <p>種類 : 人</p>
+        <p>性別 : ---</p>
+        <p>写真 :</p>
+        <p>　　　 <img src={image1} height="200px" /></p>
+        <p>名前　　 : ---</p>
+        <p>場所　　 : ---</p>
+        <p>目撃時間 : ---</p>
+        <p>特徴　　 : ---</p>
+      </div>
+      <div className="okbtn-posi">
+        <button onClick={ok} className='con-btn'>確定する</button>
+      </div>
     </div>
   )
 }
--- src/components/Home/AddPost/SureMone.jsx
+++ src/components/Home/AddPost/SureMone.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import image from "../../img/image.png"
 import { useNavigate } from 'react-router-dom'
+import sampleimg from "../../img/sampleimg.png"
 
 function SureMone() {
-  const navigation =useNavigate()
-  const back=()=>{
+  const navigation = useNavigate()
+  const back = () => {
     navigation("/login/home/addpost/mono")
   }
-  const ok=()=>{
+  const ok = () => {
     navigation("/login/home/addpost")
   }
   return (
     <div>
       <div className='blue'></div>
       <img src={image} height="50px" className='back-btn' onClick={back} />
-      <h1>情報の確認してね（もの）</h1>
-      <button className="con-btn"onClick={ok}>確定する</button>
+
+      <p style={{ fontSize: "29px", marginTop: "0px", marginBottom: "0px", marginLeft: "5%" }}>情報を確認してね​</p>
+      <div className='con-text'>
+        <p>種類 : ---</p>
+        <p>写真 :</p>
+        <p>　　　 <img src={sampleimg} height="200px" /></p>
+        <p>名前　　 : ---</p>
+        <p>場所　　 : ---</p>
+        <p>目撃時間 : ---</p>
+        <p>特徴　　 : ---</p>
+      </div>
+      <div className="okbtn-posi">
+        <button onClick={ok} className='con-btn'>確定する</button>
+      </div>
     </div>
   )
 }
