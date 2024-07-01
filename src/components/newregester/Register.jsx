51b957f649487cff32cceaaeb282f71342056bb7
yumachin <curryuma925@gmail.com>
2024-07-01 23:02:39 +0900

    新規登録画面のCSS

--- src/components/newregester/Register.jsx
+++ src/components/newregester/Register.jsx
@@ -8,26 +8,46 @@ function Register() {
 
   }
   return (
-    <div>
-      <h1 className='Regtitle'>新規登録</h1>
-      <div className='Reginput'>
-        お名前：
-        <input></input><br/>
-        フリガナ：
-        <input></input><br/>
-        性別：
-        <input type="checkbox" id = "1"/>男性
-        <input type="checkbox" id = "2"/>女性<br/>
-        電話番号：
-        <input></input><br/>
-        パスワード：
-        <input></input><br/>
-      </div><br/>
-      <div className='Reg'>
-        <button onClick={complete}>登録</button>
+    <div className="regi_background">
+      <div className='regi_rectangle'>
+        <h2 className='regi_title'>新規登録</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td className='regi_item'>お名前</td>
+              <td><input className='regi_input'></input></td>
+            </tr>
+            <tr>
+              <td className='regi_item'>フリガナ</td>
+              <td><input className='regi_input'></input></td>
+            </tr>
+            <tr>
+              <td className='regi_item'>性別</td>
+              <td>
+                <input type="radio" className='man' name='only'></input>
+                  <label className='sexal'>男性</label>
+                <input type="radio" className='woman' name='only'></input>
+                  <label className='sexal'>女性</label>
+                <input type="radio" className='no_response' name='only'></input>
+                  <label className='sexal'>回答しない</label>
+              </td>
+            </tr>
+            <tr>
+              <td className='regi_item'>電話番号</td>
+              <td><input className='regi_input'></input></td>
+            </tr>
+            <tr>
+              <td className='regi_item'>パスワード</td>
+              <td><input className='regi_input'></input></td>
+            </tr>
+          </tbody>
+        </table>
+        <p className='regi_attach'>＜顔写真の添付＞</p>
+        <input type="file" className='regi_picture'></input>
+        <button className='regi_button' onClick={complete}>登録</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Register
