cec041f6563181cb6c8106600d9dc1bb8b11b220
kondosoya <kondosoya0704@gmail.com>
2024-07-07 00:01:18 +0900

    lostdetail11

--- src/components/LostDetail/LostDeatail.jsx
+++ src/components/LostDetail/LostDeatail.jsx
@@ -5,31 +5,32 @@ import logoImage from '../LostDetail/testpict.png';
 import { AiOutlinePicture } from "react-icons/ai";
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
 import { BsPlusLg } from "react-icons/bs";
-import { IoIosExpand } from 'react-icons/io';
+import { FaRegPaperPlane } from "react-icons/fa";
+// import { doc, setDoc } from 'firebase/firestore';
+// import { IoIosExpand } from 'react-icons/io';
 
 
-const ChangePict = ({ newLogo, setLogo }) => { //写真を表示
+const ChangePict = ({ newLogo, setLogo, activePost }) => { //写真を表示
 
     setLogo(newLogo);
 
     return (
-        <img className='lostdetail-logo-image' src={newLogo || logoImage} alt="Logo" />
+        <img className='lostdetail-logo-image' src={activePost.file} alt="Logo" />
     );
 };
 
-const Tag = () => { //関連するタグを表示
+const Tag = ({ activePost }) => { //関連するタグを表示
     return (
         <div className='lostdetail-tag-container'>
             <ul className='lostdetail-tag-list'>
-                <li>tag1aaaaaaaaaaaa</li>
-                <li>tag2</li>
-                <li>tag3bbbbb</li>
+                <li>{activePost.kind}</li>
+                <li>tag</li>
             </ul>
         </div>
     );
 };
 
-const DetDisp = ({ Add, setAdd }) => { //詳細を表示
+const DetDisp = ({ Add, setAdd, activePost }) => { //詳細を表示
     const [open, setOpen] = useState(true);
 
     const detail = () => {
@@ -37,12 +38,24 @@ const DetDisp = ({ Add, setAdd }) => { //詳細を表示
         setAdd(!Add);
     };
 
+    const formatDateTime = (timestamp) => {
+        const date = timestamp.toDate();
+        const option = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formatD = date.toLocaleDateString('ja-JP', option);
+
+        const hour = date.getHours();
+        const minute = date.getMinutes().toString().padStart(2, '0');
+        const formatT = `${hour}時${minute}分`;
+
+        return `${formatD} ${formatT}`;
+    };
+
     // const Plus = () => setOpen((prev) => !prev);
 
     return (
         <>
             <div className='lostdetail-font'>
-                Show Details
+                詳細
             </div>
             <button className='lostdetail-detail-button' onClick={detail}>
                 {open ? <GoChevronUp /> : <GoChevronDown />}
@@ -50,21 +63,21 @@ const DetDisp = ({ Add, setAdd }) => { //詳細を表示
 
             <div className={`lostdetail-collapse ${open ? 'lostdetail-visible' : 'lostdetail-hidden'}`}>
                 <div className='lostdetail-section'>
-                    <div className='lostdetail-title'>特徴</div>
+                    <div className='lostdetail-title'>詳細情報</div>
                     <div className='lostdetail-content'>
-                        aaaaaaaaaaaaaaaaaaaaaa
+                        {activePost.text}
                     </div>
                 </div>
                 <div className='lostdetail-section'>
-                    <div className='lostdetail-title'>場所</div>
-                    <div className='lostdetail-content'>
-                        bbb
+                    <div className='lostdetail-title'>投稿時間</div>
+                    <div className='lostdetail-content lostdetail-timestamp'>
+                        {formatDateTime(activePost.time)}
                     </div>
                 </div>
                 <div className='lostdetail-section'>
-                    <div className='lostdetail-title'>時間</div>
+                    <div className='lostdetail-title'>その他</div>
                     <div className='lostdetail-content'>
-                        c
+
                     </div>
                 </div>
                 <div className='lostdetail-section'>
@@ -117,30 +130,39 @@ const Message = () => { //メッセージを入力
     };
 
     return (
-        <input className='lostdetail-message-button' placeholder='メッセージを入力してください'>
+        <input className='lostdetail-message-button'
+            placeholder='メッセージをココに入力'>
         </input>
     );
 };
 
+const Contribution = () => {
+    return (<button className='contbtn'>
+        <FaRegPaperPlane />
+    </button>
+    )
+}
 
-function App() {
+
+function App({ activePost }) {
 
     const [logo, setLogo] = useState(logoImage);
     const [AddDet, setAddDet] = useState(false);
+
 
 
     return (
         <div className='lostdetail-body'>
             <div className='lostdetail-container'>
                 <div className='lostdetail-box lostdetail-ctp lostdetail-top'>
-                    <ChangePict newLogo={logo} setLogo={setLogo} />
-                    <Tag />
+                    <ChangePict newLogo={logo} setLogo={setLogo} activePost={activePost} />
+                    <Tag activePost={activePost} />
                     <br />
                 </div>
 
 
                 <div className='lostdetail-box lostdetail-detail-display'>
-                    <DetDisp Add={AddDet} setAdd={setAddDet} />
+                    <DetDisp Add={AddDet} setAdd={setAddDet} activePost={activePost} />
                     <br />
                 </div>
 
@@ -149,6 +171,7 @@ function App() {
                         <Plusalpha />
                         <Camera />
                         <Message />
+                        <Contribution />
                     </div>
                 </div>
             </div>
