be3b202d300b25c0288a4b170bac1646ef629f5d
yumachin <curryuma925@gmail.com>
2024-07-06 22:55:46 +0900

    rogin-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation, useNavigate } from 'react-router-dom';
-// import Register from './components/newregester/Register';
+import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import "./App.css";
 import Username from './components/newregester/Username';
@@ -26,8 +25,8 @@ import LostDetail from "./components/LostDetail/LostDeatail";
 import SureMone from './components/Home/AddPost/SureMone';
 import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 import { signInWithPopup } from "firebase/auth";
-// import { useAuthState} from "react-firebase-hooks/auth";
 import { db, auth, provider } from './firebase';
+import anime from 'animejs/lib/anime.es.js';
 
 function App() {
   const [userData, setUserData] = useState([]);
@@ -38,34 +37,56 @@ function App() {
         const querySnapshot = await getDocs(collection(db, 'users'));
         const usersList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         setUserData(usersList);
-        
-        // console.log("All users data:", usersList);
       } catch (e) {
         console.error("Error getting documents: ", e);
       }
     };
     fetchAllUserData();
+
+    const container = document.querySelector('.rogContainer');
+    if (container) {
+      for (let i = 0; i <= 50; i++) {
+        const blocks = document.createElement('div');
+        blocks.classList.add('block');
+        container.appendChild(blocks);
+      }
+    }
+
+    const animateBlocks = () => {
+      anime({
+        targets: '.block',
+        translateX: () => anime.random(-400, 400),
+        translateY: () => anime.random(-700, 700),
+        scale: () => anime.random(1, 7),
+        duration: 5000,
+        delay: anime.stagger(10, { start: 1000 }),
+        direction: 'alternate',
+        easing: 'easeInOutQuad',
+        complete: () => {
+          setTimeout(animateBlocks, 2000);
+        },
+      });
+    };
+
+    animateBlocks();
   }, []);
 
-  const [activePost,setActivePost]=useState({})
-  const [myInfo,setMyInfo]=useState({userName:"",place:"",photoURL:""})
-  const [disInfo, setDisInfo] = useState({ kind: "", text: "", img: "" ,file:"",place:"",poster:""});
-  const [profile, setProfile] = useState({ name: "", furigana: "", gender: "", password: "", tel: "", auth: false,time:null });
+  const [activePost, setActivePost] = useState({});
+  const [myInfo, setMyInfo] = useState({ userName: "", place: "", photoURL: "" });
+  const [disInfo, setDisInfo] = useState({ kind: "", text: "", img: "", file: "", place: "", poster: "" });
+  const [profile, setProfile] = useState({ name: "", furigana: "", gender: "", password: "", tel: "", auth: false, time: null });
   const [hitoInfo, setHitoInfo] = useState({ name: "", age: "", time: "", gender: "", place: "", tokutyou: "" });
   const [petInfo, setPetInfo] = useState({ name: "", time: "", place: "", tokutyou: "" });
-  // const [register, setRegister] = useState({ tel: "", password: "", gender: "", name: "", furigana: "", username: "" ,place:""});
 
   return (
     <Router>
-      <div>
-        <Navigation userData={userData} setProfile={setProfile} /*setRegister={setRegister}*//>
+      <div className="container">
+        <Navigation userData={userData} setProfile={setProfile} />
         <Routes>
-        <Route path="/login/home" element={<Home setActivePost={setActivePost} myInfo={myInfo} setMyInfo={setMyInfo}/>} />
-        <Route path="/login" element={<Login />} />
-          {/* <Route path="/login/register" element={<Register register={register} setRegister={setRegister} />} /> */}
-          <Route path="/login/username" element={<Username  />} />
-          <Route path="/login/serchplace" element={<SerchPlace/>} />
-          {/* <Route path="/login/serchplace" element={<SerchPlace register={register} setRegister={setRegister}/>} /> */}
+          <Route path="/login/home" element={<Home setActivePost={setActivePost} myInfo={myInfo} setMyInfo={setMyInfo} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/username" element={<Username />} />
+          <Route path="/login/serchplace" element={<SerchPlace />} />
           <Route path="/login/namekanji" element={<Name />} />
           <Route path="/login/namefurigana" element={<Namefuri />} />
           <Route path="/login/gender" element={<Gender />} />
@@ -79,130 +100,58 @@ function App() {
           <Route path="/login/home/addpost/hito/SureHitoInfo" element={<SureHitoInfo hitoInfo={hitoInfo} setHitoInfo={setHitoInfo} />} />
           <Route path="/login/home/addpost/pet" element={<Pet petInfo={petInfo} setPetInfo={setPetInfo} />} />
           <Route path="/login/home/addpost/pet/surepet" element={<SurePet petInfo={petInfo} setPetInfo={setPetInfo} />} />
-          <Route path="/login/home/addpost/mono" element={<Mono disInfo={disInfo} setDisInfo={setDisInfo}/>} />
-          <Route path="/login/home/addpost/mono/suremono" element={<SureMone disInfo={disInfo} setDisInfo={setDisInfo} myInfo={myInfo}/>} />
+          <Route path="/login/home/addpost/mono" element={<Mono disInfo={disInfo} setDisInfo={setDisInfo} />} />
+          <Route path="/login/home/addpost/mono/suremono" element={<SureMone disInfo={disInfo} setDisInfo={setDisInfo} myInfo={myInfo} />} />
           <Route path="/login/home/message" element={<Messeage />} />
           <Route path="/login/home/chat" element={<Chat />} />
-          <Route path="/login/home/finder" element={<LostDetail activePost={activePost}/>} />
+          <Route path="/login/home/finder" element={<LostDetail activePost={activePost} />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-
-function Navigation(/*{ userData, setProfile, setRegister }*/) {
+function Navigation() {
   const location = useLocation();
   const navigate = useNavigate();
-  // const [tel, setTel] = useState("");
-  // const [password, setPassword] = useState("");
 
-  // const [user] = useAuthState(auth);
-
-  // const login = () => {
-  //   const user = userData.find(user => user.tel === tel && user.password === password);
-  //   if (user) {
-  //     // console.log(user.name)
-  //     navigate('/login/serchplace');
-  //     localStorage.setItem("電話番号", JSON.stringify(user.tel));
-  //     setRegister( {tel: user.tel, password: user.password, gender: user.gender, name: user.name, furigana: user.furigana, username: user.username ,place:user.place})
-  //     setProfile({ name: user.name, furigana: user.furigana, gender: user.gender, password: user.password, tel: user.tel });
-  //   } else {
-  //     window.alert("正しくないです");
-  //   }
-  // };
-
-  // /login パス以降の場合はナビゲーションを表示しない
   if (location.pathname.startsWith('/login')) {
     return null;
   }
 
-  /* googleログイン */
-  function SignInButton(){
+  function SignInButton() {
     const signInWithGoogle = () => {
       signInWithPopup(auth, provider)
-      .then(async (result) => {
-        navigate('/login/serchplace');
-        // console.log('signin', result.user);
+        .then(async (result) => {
+          navigate('/login/serchplace');
 
-        const user = result.user;
-        const userData = {
-          uid: user.uid,
-          displayName: user.displayName,
-          email: user.email,
-          photoURL: user.photoURL
-        };
-        localStorage.setItem("uid",user.uid)
-        const docRef = doc(db, 'googleusers', user.uid);
-        await setDoc(docRef, userData);
-      }).catch((error) => {
-        console.log('signin error')
-      })
+          const user = result.user;
+          const userData = {
+            uid: user.uid,
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL
+          };
+          localStorage.setItem("uid", user.uid);
+          const docRef = doc(db, 'googleusers', user.uid);
+          await setDoc(docRef, userData);
+        }).catch((error) => {
+          console.log('signin error');
+        });
     };
 
-    return(
-      <button onClick={signInWithGoogle}>  
-        <p>グーグルでサインイン</p>
+    return (
+      <button className='rogButton' onClick={signInWithGoogle}>  
+        <p> Googleでサインイン</p>
       </button>
     )
   }
 
-  // const SerchPlace = () => {
-  //   navigate('/login/serchplace');
-  // }
-
-  // function SignOutButton(){
-  //   return(
-  //     <button onClick={() => auth.signOut()}/>
-  //   )
-  // }
-  // function UserInfo(){
-  //   return(
-  //     <div>
-  //       <img src={auth.currntUser.photoURL} alt=""/>
-  //       <p>{auth.currntUser.diplayName}</p>
-  //     </div>
-  //   )
-  // }
-
   return (
-    <div>
-      <h2 className="title">Figare</h2>
-      <div className='black-line'></div>
-      <h1 className='subtitle'>ログイン</h1>
+    <div className='rogContainer'>
+      <h2 className="rogTitle">Figare</h2>
       <nav className='mainlogin'>
-        {/* <div className='Input'>
-          <div className='input'>
-            電話番号
-            <input type='number' onChange={(e) => setTel(e.target.value)} />
-          </div>
-          <br />
-          <div className='input'>
-            パスワード
-            <input type='password' onChange={(e) => setPassword(e.target.value)} />
-          </div>
-        </div>
-        <br />
-        <div id="login">
-          <button onClick={login} className='login'>ログイン</button>
-        </div> */}
-
-        <SignInButton/>
-
-        {/* <div>
-          {user ?(
-            <>
-            <SerchPlace/>
-            </>
-          ) : (
-            <SignInButton/>
-          )}
-        </div> */}
-
-        {/* <br />
-        <div className='new'>
-          <Link to="/login/register">新規登録の方はこちらへ</Link>
-        </div> */}
+        <SignInButton />
       </nav>
     </div>
   );
