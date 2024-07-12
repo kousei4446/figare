e66fdf0ff41924e352f70ffa88d7fa65da77752b
yumachin <curryuma925@gmail.com>
2024-07-12 17:13:24 +0900

    a

--- src/components/Home/AddPost/Mono.jsx
+++ src/components/Home/AddPost/Mono.jsx
@@ -4,6 +4,7 @@ import "./Mono.css";
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage, db } from '../../../firebase';
 import { v4 as uuidv4 } from 'uuid';
+import { IoMdArrowRoundBack } from 'react-icons/io';
 
 function Mono({ disInfo, setDisInfo }) {
   const navigate = useNavigate();
