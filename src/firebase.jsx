22f3dd48723d5d023732b502e918a40c35d22fdb
kousei <kousei20030623@icloud.com>
2024-07-18 16:38:05 +0900

    jgrio

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,11 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDAh5RRn6k0YzRii5SLEhcrGkPH9WQBiH0",
   authDomain: "figare.firebaseapp.com",
@@ -18,15 +21,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Initialize Firebase Authentication and get a reference to the service
+const db =getFirestore(app);
+const storage=getStorage(app);;
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-// Initialize Cloud Storage and get a reference to the service
-const storage = getStorage(app);
-
-export { auth, db, storage, analytics };
+export {db, storage, auth, provider, };
